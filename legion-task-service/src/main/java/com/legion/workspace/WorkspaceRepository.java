f893146937e56c3dc23e3dd013b3b5b237a5f0bc
kurocifer <draculemihawk232@gmail.com>
2025-12-14 21:51:13 +0100

    Create Repository classes for entites

---
+++ legion-task-service/src/main/java/com/legion/workspace/WorkspaceRepository.java
@@ -0,0 +1,14 @@
+package com.legion.workspace;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+
+    Optional<Workspace> findBySlug(String slug);
+
+    boolean existsSlug(String slug);
+}
